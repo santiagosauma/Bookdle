73f2dc9030612075bed2ddd42fc0d279ec49f9ff
SantiagoSauma <lssaumap@gmail.com>
2024-08-19 10:47:54 -0600

    Board Structure created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+import React, { useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
-    <div className="App"></div>
+    <div className="App"><nav>
+      <h1>Bookdle</h1>
+    </nav>
+    <AppContext.Provider value={{board, setBoard}}>
+      <Board />
+      <Keyboard />
+    </AppContext.Provider>
+    </div>
   );
 }
 
