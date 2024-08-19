7df65d56d815d9527b067381851b29ca24989463
SantiagoSauma <lssaumap@gmail.com>
2024-08-19 10:51:45 -0600

    Grid Structure Fixed

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -6,7 +6,7 @@ function Letter({ letterPos, attemptVal }) {
   const { board} =
     useContext(AppContext);
   const letter = board[attemptVal][letterPos];
-  return <div className="letter">Letter</div>
+  return <div className="letter">{letter}</div>
 }
 
 export default Letter
